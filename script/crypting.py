cb37674e00d0e00b3a7970e0c88bb00b35f40ac9
Janek Jahnke <jahjah1907@gmail.com>
2023-02-09 18:13:14 +0100

    crypting

--- script/crypting.py
+++ script/crypting.py
@@ -1,40 +1,47 @@
 from cryptography.fernet import Fernet
 
 class Crypting():
-    def encrypt():
+    # encrypting file
+    def encrypt(path: str):
+        if not path.endswith("\\"):
+            path = path + "\\"
+
         key = Fernet.generate_key()
 
-        with open("F:\\programming\\PYprojects\idkGame4\\filekey.key", "wb") as filekey:
+        with open(path + "filekey.key", "wb") as filekey:
             filekey.write(key)
         filekey.close()
 
-        with open ("F:\\programming\\PYprojects\idkGame4\\filekey.key", "rb") as filekey:
+        with open (path + "filekey.key", "rb") as filekey:
             key = filekey.read()
         filekey.close()
 
         fernet = Fernet(key)
 
-        with open("F:\\programming\\PYprojects\\idkGame4\\script\\data.json", "rb") as file:
+        with open(path + "data.json", "rb") as file:
             original = file.read()
         file.close()
 
         encrypted = fernet.encrypt(original)
 
-        with open("F:\\programming\\PYprojects\\idkGame4\\script\\data.json", "wb") as encrypted_file:
+        with open(path + "data.json", "wb") as encrypted_file:
             encrypted_file.write(encrypted)
         encrypted_file.close()
-
-        return fernet
     
+    # decypting file
+    def decrypt(path: str):
+        if not path.endswith("\\"):
+            path = path + "\\"
 
-    def decrypt(fernet: Fernet):
+        with open(path + "filekey.key") as keyfile:
+            key = keyfile.read()
 
-        with open("F:\\programming\\PYprojects\\idkGame4\\script\\data.json", "rb") as enc_file:
+        fernet = Fernet(key)
+
+        with open(path + "data.json", "rb") as enc_file:
             encrypted = enc_file.read()
 
         decrypted = fernet.decrypt(encrypted)
 
-        with open("F:\\programming\\PYprojects\\idkGame4\\script\\data.json", "wb") as dec_file:
+        with open(path + "data.json", "wb") as dec_file:
             dec_file.write(decrypted)
-
-Crypting.encrypt()
