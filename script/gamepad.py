a34b0a6aa1f688d054adcf2ac0ef1332ebe6f162
Janek Jahnke <jahjah1907@gmail.com>
2023-02-11 15:35:08 +0100

    PS5 controller support added

--- script/gamepad.py
+++ script/gamepad.py
@@ -5,30 +5,7 @@ class Inputs():
 
     def scan():
         """
-        PS5:
-        X:                     10,   pressed = 1, released = 0
-        O:                     11,   pressed = 1, released = 0
-        □:                     12,   pressed = 1, released = 0
-        ▲:                     13,   pressed = 1, released = 0
-        L1:                    19,   pressed = 1, released = 0
-        R1:                    20,   pressed = 1, released = 0
-        L2:                     7, range -1 to 1, pressed 1, released -1
-        R2:                     8, range -1 to 1, pressed 1, released -1
-        CREATE:                14,   pressed = 1, released = 0
-        PS-BUTTON:             15,   pressed = 1, released = 0
-        OPTIONS:               16,   pressed = 1, released = 0
-        TOUCHPAD:              25,   pressed = 1, released = 0
-        DIRECTIONAL-UP:        21,   pressed = 1, released = 0
-        DIRECTIONAL-DOWN:      22,   pressed = 1, released = 0
-        DIRECTIONAL-LEFT:      23,   pressed = 1, released = 0
-        DIRECTIONAL-RIGHT:     24,   pressed = 1, released = 0
-        LEFT-STICK-UP/DOWN:     4, range -1 to 1, down 1, up -1
-        LEFT-STICK-LEFT/RIGHT:  3, range -1 to 1, right 1, left -1
-        L3:                    17,   pressed = 1, released = 0
-        RIGHT-STICK-UP/DOWN:    6, range -1 to 1, down 1, up -1
-        RIGHT-STICK-LEFT/RIGHT: 5, range -1 to 1, right 1, left -1
-        R3:                    18,   pressed = 1, released = 0
-        MUTE-BUTTON:           26,   pressed = 1, released = 0
+        Supports PS5 and Nintendo Switch Pro Controller
         """
         pygame.joystick.init()
         a = 0
@@ -47,6 +24,9 @@ class Inputs():
                 #dataString.append("Joystick {}".format(i))
         
                 name = joystick.get_name()
+                if name == "Nintendo Switch Pro Controller":
+                    pass
+
                 data.append(name)
                 dataString.append("Joystick name: {}".format(name))
         
