ddcb30396fb913e0611ad6b8ddfe6dff046a90a4
Janek Jahnke <jahjah1907@gmail.com>
2023-03-07 17:47:38 +0100

    better coding for whole game

--- script/game.py
+++ script/game.py
@@ -3,10 +3,10 @@ import pygame
 import sys
 import json
 import os
-import random
 
+from npc import NPC
+from player import Player
 from expandList import ExpandList
-from pygame_merge import Merge
 from crypting import Crypting
 from gamepad import Inputs
 
@@ -19,11 +19,13 @@ class Game():
         pygame.init()
         pygame.font.init()
 
-        # getting paths for json; msgbox; enemy
+        # getting paths
         Game.jsonPath = Game.path("script")
         Game.msgboxPath = Game.path("sprites", "msgboxes")
         Game.enemyPath = Game.path("sprites", "enemies")
         Game.mergePath = Game.path("temp")
+        Game.npc_path = Game.path("sprites", "NPCs")
+        Game.playerPath = Game.path("sprites", "player")
 
         # encrypting the json
         Game.cryptingPath = Game.path("script")
@@ -86,7 +88,7 @@ class Game():
         Game.pause = Pause()
         Game.battle = Battle()
 
-        Game.NPC1 = NPC((500, 1768), Game.camera)
+        NPC((500, 1768), Game.npc_path, Game.camera)
         Game.playerLoaded = False
 
         Enemy.list = []
@@ -176,7 +178,7 @@ class Game():
 
     # creates a path to files
     def path(newPath: str = None, newPath2: str = None):
-        absolutePath = os.path.abspath(__file__)
+        absolutePath = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
         fileDirectory = os.path.dirname(absolutePath)
         parentDirectory = os.path.dirname(fileDirectory)
         # parentDirectory = os.path.join(parentDirectory, Game.getFileName()) # remove comment when converted to an exe and comment when run in editor------------------------------------------------------------------
@@ -313,328 +315,6 @@ class Game():
         pygame.display.update()
         self.clock.tick(20)
 
-
-class NPC(pygame.sprite.Sprite):
-    # initializing
-    def __init__(self, pos, group):
-        super().__init__(group)
-
-        NPC.hitted = False
-        NPC.path = Game.path("sprites", "NPCs")
-
-        NPC.allItems = []
-        NPC.image = pygame.image.load(NPC.path + NPC.spawn("testNPC")).convert_alpha()
-        NPC.rect = NPC.image.get_rect(topleft=pos)
-
-    # spawn the npc
-    def spawn(item: str):
-        for file in os.listdir(NPC.path):
-            casefoldFile = file[:-4].casefold()
-            item = item.casefold()
-            if casefoldFile == item:
-                return file
-
-    # check if npc is hit
-    def hit():
-        collision_x = Player.rect[0] + 64 >= NPC.rect[0] and NPC.rect[0] + 64 >= Player.rect[0]
-        collision_y = Player.rect[1] + 64 >= NPC.rect[1] and NPC.rect[1] + 64 >= Player.rect[1]
-        return collision_y and collision_x
-
-
-class MsgBox(pygame.sprite.Sprite):
-    # initializing
-    def __init__(self, pos, text: str, font: str, fontSize: int, rgb: tuple, msgbox: str, group):
-        super().__init__(group)
-
-        font_ = pygame.font.SysFont(font, fontSize)
-        box = pygame.image.load(Game.msgboxPath + msgbox)
-        text_ = font_.render(text, True, rgb)
-        self.image = Merge.surfaces(Game.mergePath, box, text_)
-        self.rect = self.image.get_rect(topleft=pos)
-
-    # NPC Msgbox
-    def NPC(text: str, font: str, fontSize: int, rgb: tuple):
-        MsgBox((NPC.rect[0] - 224, NPC.rect[1] - 192), text, font, fontSize, rgb, "test.png", Game.camera)
-
-    # Cutszene Msgbox
-    def Cutszene():
-        pass
-
-
-class HUD(pygame.sprite.Sprite):
-    # initializing
-    def __init__(self, group, pos, text: str, font: str, fontSize: int, rgb: tuple):
-        super().__init__(group)
-
-        font_ = pygame.font.SysFont(font, fontSize)
-        HUD.image = font_.render(text, True, rgb)
-        HUD.rect = HUD.image.get_rect(center=pos)
-
-
-    def updateHUD(text: str, font: str, fontSize: int, rgb: tuple):
-        Player.HUD_w = Player.rect.center[0] + (Game.screen.get_size()[0] // 2) - 125
-        Player.HUD_h = Player.rect.center[1] - (Game.screen.get_size()[1] // 2) + 25
-        pos = (Player.HUD_w, Player.HUD_h)
-
-        font_ = pygame.font.SysFont(font, fontSize)
-        HUD.image = font_.render(text, True, rgb)
-        HUD.rect = HUD.image.get_rect(center=pos)
-
-
-
-class Player(pygame.sprite.Sprite):
-    # initializing
-    def __init__(self, pos, group):
-        super().__init__(group)
-
-        Player.i = 0
-        Player.LVL = Game.playerdata["LVL"]
-        Player.XP = Game.playerdata["XP"]
-        Player.HP = Game.playerdata["HP"]
-        Player.PHYATK = Game.playerdata["PHYATK"]
-        Player.MAGATK = Game.playerdata["MAGATK"]
-        Player.PHYDEF = Game.playerdata["PHYDEF"]
-        Player.MAGDEF = Game.playerdata["MAGDEF"]
-        Player.SPEED = Game.playerdata["SPEED"]
-        Player.STAMINA = Game.playerdata["STAMINA"]
-
-        Player.path = Game.path("sprites", "player")
-        Player.right = "playerR.png"
-        Player.left = "playerL.png"
-        Player.rightB = "playerRblack.png"
-        Player.leftB = "playerLblack.png"
-        Player.facingLeft = False
-        Player.facingRight = True
-        Player.hit = True
-
-        Player.color = "w" # w: white; b: black
-
-        Player.image = pygame.image.load(Player.path + Player.right).convert_alpha()
-        Player.rect = Player.image.get_rect(center=pos)
-        self.direction = pygame.math.Vector2()
-        self.fall = pygame.math.Vector2()
-        self.jumpV = pygame.math.Vector2()
-        self.standard = 10
-        self.speed = self.standard
-
-        self.fallingspeed = 10
-        self.fall.y = self.fallingspeed
-        self.fall.x = 0
-
-        self.jumpcount = 30
-        self.jumping = False
-        
-        Player.attack = Attack(Game.camera, "PHY", 55)
-
-        Player.HUD_w = 0
-        Player.HUD_h = 0
-        Player.HUD = HUD(Game.camera, (Player.HUD_w, Player.HUD_h), f"Stamina: {Player.STAMINA}", None, 60, (255, 0, 0))
-        
-   # drains the players stamina
-    def drain(self):
-        timer = Game.tracktime(Game.playerHitTicks, Game.playerHitSeconds, Game.playerHitMinutes, Game.playerHitHours)
-        if Player.color == "b" and Player.STAMINA > 0:
-            Game.playerHitTicks = timer["ticks"]
-            Game.playerHitSeconds = timer["seconds"]
-
-            if timer["seconds"] % 2 == 0:
-                Player.STAMINA -= 1
-                Game.playerHitTicks = 0
-                Game.playerHitSeconds = 0
-        if Player.STAMINA == 0:
-            Player.color = "w"
-            self.playerImage()
-        if Player.color == "w" and Player.STAMINA < Game.playerdata["STAMINA"]:
-            if timer["seconds"] % 2 == 0:
-                Player.STAMINA += 1
-                Game.playerHitTicks = 0
-                Game.playerHitSeconds = 0
-        if Player.color == "b":
-            self.speed = self.standard * 1.5
-        else:
-            self.speed = self.standard
-    
-    # jumping
-    def jump(self):
-        self.keys = pygame.key.get_pressed()
-        if (self.keys[pygame.K_SPACE] or (Game.gamepadInputs != None and Game.gamepadInputs[10] == 1)) and not self.jumping:
-            if self.jumpcount >= 1:
-                Player.rect.center -= self.fall
-                self.jumpcount -= 1
-            if self.jumpcount == 0:
-                self.jumping = True          
-
-        elif Player.rect.center[1] <= 1800 - self.fall.y:
-            self.jumpcount = 0
-            if Player.rect.center[1] == 1800 - self.fall.y:
-                self.jumping = False
-                self.jumpcount = 30
-            self.fall.y = self.fallingspeed
-            Player.rect.center += self.fall
-
-    # set controls for gamepad movement
-    def gamepad(self):
-        """
-        try:
-            if Game.gamepadInputs[4] > 0:
-                if self.col_bottom == False:
-                    self.direction.y = Game.gamepadInputs[4]
-                else:
-                    self.direction.y = 0
-            elif Game.gamepadInputs[4] < 0:
-                if self.col_top == False:
-                    self.direction.y = Game.gamepadInputs[4]
-                else:
-                    self.direction.y = 0
-        """
-        try:
-            if Game.gamepadInputs[3] > 0:
-                if self.col_right == False:
-                    Player.facingRight = True
-                    Player.facingLeft = False
-                    self.direction.x = Game.gamepadInputs[3]
-                else:
-                    self.direction.x = 0
-            elif Game.gamepadInputs[3] < 0:
-                if self.col_left == False:
-                    Player.facingRight = False
-                    Player.facingLeft = True
-                    self.direction.x = Game.gamepadInputs[3]
-                else:
-                    self.direction.x = 0
-        except:
-            pass
-
-    # set the keys for movement
-    def keyboard(self):
-        self.keys = pygame.key.get_pressed()
-
-        """
-        if self.keys[pygame.K_w]:
-            if self.col_top == False:
-                self.direction.y = -1
-            else:
-                self.direction.y = 0
-        elif self.keys[pygame.K_s]:
-            if self.col_bottom == False:
-                self.direction.y = 1
-            else:
-                self.direction.y = 0
-        else:
-            self.direction.y = 0
-        """
-
-        if self.keys[pygame.K_d]:
-            Player.facingRight = True
-            Player.facingLeft = False
-            if self.col_right == False:
-                self.direction.x = 1
-            else:
-                self.direction.x = 0
-        elif self.keys[pygame.K_a]:
-            Player.facingRight = False
-            Player.facingLeft = True
-            if self.col_left == False:
-                self.direction.x = -1
-            else:
-                self.direction.x = 0
-        else:
-            self.direction.x = 0
-
-    
-    def playerImage(self):
-        if Player.facingLeft == True:
-            if Player.color == "w":
-                Player.image = pygame.image.load(Player.path + Player.left).convert_alpha()
-            elif Player.color == "b" and Player.STAMINA > 0:
-                Player.image = pygame.image.load(Player.path + Player.leftB).convert_alpha()
-        elif Player.facingRight == True:
-            if Player.color == "w":
-                Player.image = pygame.image.load(Player.path + Player.right).convert_alpha()
-            elif Player.color == "b" and Player.STAMINA > 0:
-                Player.image = pygame.image.load(Player.path + Player.rightB).convert_alpha()
-
-        if (self.keys[pygame.K_TAB] and Game.ticksToIgnoreTAB == 0) or (Game.gamepadInputs != None and Game.gamepadInputs[13] == 1 and Game.ticksToIgnoreTAB == 0):
-            Game.ticksToIgnoreTAB = 30
-            if Player.color == "w":
-                Player.color = "b"
-            elif Player.color == "b":
-                Player.color = "w"
-
-        if Game.ticksToIgnoreTAB > 0:
-            Game.ticksToIgnoreTAB -= 1
-
-    # updating details
-    def update(self):
-        if self.screen_collision_left():
-            self.col_left = True
-        else:
-            self.col_left = False
-        if self.screen_collision_right():
-            self.col_right = True
-        else:
-            self.col_right = False
-        if self.screen_collision_top():
-            self.col_top = True
-        else:
-            self.col_top = False
-        if self.screen_collision_bottom():
-            self.col_bottom = True
-        else:
-            self.col_bottom = False
-
-        if NPC.hit() and NPC.hitted == False:
-            NPC.hitted = True
-            MsgBox.NPC("This is a demo text", None, 60, (66, 135, 245))
-
-        self.keyboard()
-        self.gamepad()
-        self.playerImage()
-        self.drain()
-        self.jump()
-
-        Player.rect.center += self.direction * self.speed
-        Attack.input(self)
-        HUD.updateHUD(f"Stamina: {Player.STAMINA - 1}", None, 60, (255, 0, 0))
-        Enemy.attackPlayer(self)
-        Game.teleport(self, "ground:0", "ground:1", 1948, 900, 52, 200)
-
-    # collision with 2 objects
-    def aabb_collision(self, a_x, a_y, a_width, a_height, b_x, b_y, b_width, b_height):
-        self.collision_x = a_x + a_width >= b_x and b_x + b_width >= a_x
-        self.collision_y = a_y + a_height >= b_y and b_y + b_height >= a_y
-        return self.collision_y and self.collision_x
-
-    # left screen collision
-    def screen_collision_left(self):
-        return Player.rect.center[0] <= 0
-
-    # right screen collision
-    def screen_collision_right(self):
-        return Player.rect.center[0] >= Camera.basic_width
-
-    # top screen collision
-    def screen_collision_top(self):
-        return Player.rect.center[1] <= 0
-
-    # bottom screen collision
-    def screen_collision_bottom(self):
-        return Player.rect.center[1] >= Camera.basic_height
-
-    # player enemy collision
-    def player_enemy_collision(self):
-        try:
-            enemyToCheck = Enemy.list.pop(0)
-            Enemy.list.append(enemyToCheck)
-            return self.aabb_collision(Player.rect.center[0], Player.rect.center[1], 64, 64, enemyToCheck.rect.center[0], enemyToCheck.rect.center[1], 64, 64)
-        except:
-            pass
-
-    # player teleport collision
-    def player_teleport_collision(self, hitboxX: int, hitboxY: int, hitboxWidth: int, hitboxHeigth: int):
-        return self.aabb_collision(Player.rect.center[0], Player.rect.center[1], 64, 64, hitboxX, hitboxY, hitboxWidth, hitboxHeigth)
-
-
 class Camera(pygame.sprite.Group):
     # initializing
     def __init__(self):
@@ -697,7 +377,7 @@ class Camera(pygame.sprite.Group):
                 del Camera.spriteList[i - 1]
             if str(type(Camera.spriteList[1])).partition(".")[2].split("'")[0] == "Attack":
                 del Camera.spriteList[1]
-            if NPC.hit() == False and obj == "MsgBox":
+            if NPC.hit(Player) == False and obj == "MsgBox":
                 del Camera.spriteList[4]
             if str(type(Camera.spriteList[0])).partition(".")[2].split("'")[0] == "MsgBox":
                 del Camera.spriteList[0]
@@ -960,7 +640,7 @@ class Pause():
             Game.ticksToIgnoreSPACE = 5
             if self.options_mainmenu == 1:
                 if Game.playerLoaded == False:
-                    Game.player = Player((640, 1780), Game.camera)
+                    Game.player = Player((640, 1780), Game.jsonPath, Game.playerPath, Game.camera)
                     Game.playerLoaded = True
                 Player.rect.center = (640, 1780)
                 Game.run = "game"
---
+++ script/msgbox.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame_merge import Merge
+
+class MsgBox(pygame.sprite.Sprite):
+    # initializing
+    def __init__(self, pos, text: str, font: str, fontSize: int, rgb: tuple, msgbox: str, msgboxPath: str, mergePath: str, group):
+        super().__init__(group)
+
+        font_ = pygame.font.SysFont(font, fontSize)
+        box = pygame.image.load(msgboxPath + msgbox)
+        text_ = font_.render(text, True, rgb)
+        self.image = Merge.surfaces(mergePath, box, text_)
+        self.rect = self.image.get_rect(topleft=pos)
